4328fd94c756246b54083975d92257051cb4eaf5
Tanishq Singh <tanishqsingh640@gmail.com>
2024-07-04 08:38:17 +0000

    loader added

--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -3,9 +3,11 @@ import "./style.css";
 import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
 import { Square } from "react-chessboard/dist/chessboard/types";
+import { ColorRing } from "react-loader-spinner";
 
 export function Home() {
 	const [game, setGame] = useState(new Chess());
+	const [turn, setTurn] = useState<"human" | "ai">("human");
 
 	function onDrop(from: Square, to: Square) {
 		try {
@@ -15,6 +17,8 @@ export function Home() {
 			setGame(gameCopy);
 
 			if (result) {
+				setTurn("ai");
+
 				new Promise(async () => {
 					const move = await fetch(
 						"https://chess-stockfish-yokuioanna-el.a.run.app/gen_move",
@@ -29,6 +33,7 @@ export function Home() {
 
 					const { fen } = await move.json() as { fen: string };
 
+					setTurn("human");
 					setGame(new Chess(fen));
 				});
 			}
@@ -40,10 +45,37 @@ export function Home() {
 	}
 
 	return (
-		<Chessboard
-			position={game.fen()}
-			boardWidth={324}
-			onPieceDrop={onDrop}
-		/>
+		<div>
+			<div class="person">
+				<span>AI</span>
+				<div>
+					<ColorRing
+						visible={turn === "ai"}
+						height="30"
+						width="30"
+						ariaLabel="color-ring-loading"
+						wrapperStyle={{}}
+						wrapperClass="color-ring-wrapper"
+						colors={[
+							"#e15b64",
+							"#f47e60",
+							"#f8b26a",
+							"#abbd81",
+							"#849b87",
+						]}
+					/>
+				</div>
+			</div>
+			<Chessboard
+				position={game.fen()}
+				boardWidth={324}
+				onPieceDrop={onDrop}
+			/>
+			<div class="person">
+				<span>
+					YOU {turn === "human" && (<i>(your turn)</i>)}
+				</span>
+			</div>
+		</div>
 	);
 }
